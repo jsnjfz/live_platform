2d31286ac2fdd00510cdc52b3e055cc37a7ba109
fanzhou <jsnjfz@gmail.com>
2017-03-09 20:05:51 +0800

    增加熊猫TV爬虫

--- live_platform/spiders/panda.py
+++ live_platform/spiders/panda.py
@@ -18,26 +18,29 @@ class BooksSpider(scrapy.Spider):
                                  callback=self.parse_innerurl)
 
     def parse_innerurl(self, response):
+        room_api = 'http://www.panda.tv/api_room'
         for href in response.xpath("//a[@class='video-list-item-wrap']/@href").extract():
-            yield scrapy.Request(response.urljoin(href),
+            print room_api + "?roomid=" + href[1:]
+            yield scrapy.Request(room_api + "?roomid=" + href[1:],
                                  callback=self.parse_content)
 
     def parse_content(self, response):
+        pass
         # print "*******" + response.xpath("//div[@class='rich_media_content ']").extract_first().encode('GB18030') + "*******"
         # title = response.xpath("//h1[@class='room-head-info-title']/text()").extract_first().strip('\n')
-        name = response.xpath("//span[@class='room-head-info-hostname']/text()").extract_first()
-        # date = response.xpath("//*[@id='post-date']/text()").extract_first()
-        watch_num = response.xpath("//div[@class='room-viewer-num']/span[1]/text()").extract_first()
-        follow_num = response.xpath("//span[@class='w-room-title_favnum c-icon_favnum']/text()").extract_first()
-
-        # print "****title:****" + title
-        print "****name:****" + name
-        print "****watch_num:****" + watch_num
-        print "****follow_num:****" + follow_num
-
-        with open("xiongmao", 'a+') as f:
-            f.write("\n" + name + watch_num + follow_num + "\n")
-        self.log('Saved file test')
+        # name = response.xpath("//span[@class='room-head-info-hostname']/text()").extract_first()
+        # # date = response.xpath("//*[@id='post-date']/text()").extract_first()
+        # watch_num = response.xpath("//div[@class='room-viewer-num']/span[1]/text()").extract_first()
+        # follow_num = response.xpath("//span[@class='w-room-title_favnum c-icon_favnum']/text()").extract_first()
+        #
+        # # print "****title:****" + title
+        # print "****name:****" + name
+        # print "****watch_num:****" + watch_num
+        # print "****follow_num:****" + follow_num
+        #
+        # with open("xiongmao", 'a+') as f:
+        #     f.write("\n" + name + watch_num + follow_num + "\n")
+        # self.log('Saved file test')
 
         # page = response.url.split("/")[-1]
         # title = title.replace("/", "-").replace("?", "-").replace("?", "-").replace(":", "-").replace("*", "-").replace("<", "-").replace(">", "-").replace('"', '-')
